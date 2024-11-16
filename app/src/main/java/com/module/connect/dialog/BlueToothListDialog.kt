b8cd3e0b95f57e5ea80125b61af3737e1600570c
easonLee <804645701@qq.com>
2024-11-16 14:01:45 +0800

    移除多余代码

--- app/src/main/java/com/module/connect/dialog/BlueToothListDialog.kt
+++ app/src/main/java/com/module/connect/dialog/BlueToothListDialog.kt
@@ -1,14 +1,10 @@
 package com.module.connect.dialog
 
-import android.app.ProgressDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.blankj.utilcode.util.ToastUtils
 import com.module.connect.adapter.BlueToothAdapter
 import com.module.connect.bean.BlueToothBean
 import com.module.connect.databinding.DialogBlueToothListBinding
@@ -50,30 +46,7 @@ class BlueToothListDialog : BaseFragmentDialog<DialogBlueToothListBinding>() {
 
     override fun initView(savedInstanceState: Bundle?) {
         mBluetoothAdapter = BlueToothAdapter {
-//            connectingProgressDialog = ProgressDialog(requireContext()).apply {
-//                setMessage("连接中...")
-//                setCancelable(false)
-//                show()
-//            }
-//            ConnectUtil.CURRENT_ADDRESS = it.address
-//            ConnectUtil.CURRENT_DEVICE = it.device
-//            KeyValueUtils.setString(IConsts.KEY_CURRENT_ADDRESS, it.address)
-//            ConnectUtil.CURRENT_BLUE_SOCKET = ConnectUtil.connectToGnssDevice(it.address)
-//            ConnectUtil.pairBluetoothDevice(requireContext(), it.device)
-            if (BluetoothHelper.isDevicePaired(it.device)) {
-                Toast.makeText(context,"此设备已配对", Toast.LENGTH_SHORT).show()
-            } else {
-                BluetoothHelper.connectDevice(requireContext(), it.device, {
-                    Toast.makeText(context,"连接成功", Toast.LENGTH_SHORT).show()
-                    Log.e("---", "已连接")
-                }, {
-                    Toast.makeText(context,"连接失败", Toast.LENGTH_SHORT).show()
-                    Log.e("---", "连接失败")
-                })
-            }
-
             dismissAllowingStateLoss()
-
         }
         binding.rvBlueTooth.adapter = mBluetoothAdapter
         binding.rvBlueTooth.layoutManager = LinearLayoutManager(requireContext())
--- app/src/main/java/com/module/connect/fragment/HomeFragment.kt
+++ app/src/main/java/com/module/connect/fragment/HomeFragment.kt
@@ -1,13 +1,7 @@
 package com.module.connect.fragment
 
-import BluetoothHelper
 import android.annotation.SuppressLint
-import android.app.ProgressDialog
 import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothGatt
-import android.bluetooth.BluetoothGattCallback
-import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothProfile
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
@@ -18,15 +12,11 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import com.blankj.utilcode.util.ToastUtils
 import com.module.connect.bean.BlueToothBean
 import com.module.connect.consts.IConsts
 import com.module.connect.databinding.FragmentHomeBinding
 import com.module.connect.dialog.BlueToothListDialog
 import com.module.connect.dialog.ResultDialog
-import com.module.connect.util.BluetoothLEUtil
-import com.module.connect.util.CommandUtil
-import com.module.connect.util.ConnectUtil
 import com.module.connect.util.KeyValueUtils
 import com.module.connect.util.PermissionComplianceManager
 import kotlinx.coroutines.delay
@@ -59,214 +49,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun initView() {
-        PermissionComplianceManager.requestBlueToothPermissionHasTip(requireActivity(), object :
-            PermissionComplianceManager.SimpleCallbackProxy() {
 
-            override fun onGranted() {
-                super.onGranted()
-                BluetoothHelper.init(requireContext())
-            }
-
-            override fun onDenied() {
-                super.onDenied()
-                Toast.makeText(context,"请授予蓝牙权限", Toast.LENGTH_SHORT).show()
-                requireActivity().finish()
-            }
-        })
-
-        binding.ll1.setOnClickListener {
-            val uuid = KeyValueUtils.getString(IConsts.KEY_CURRENT_WRITE_UUID)
-            val char = KeyValueUtils.getString(IConsts.KEY_CURRENT_WRITE_CHARACTERISTICS)
-            Log.e("---", "uuid：$uuid")
-            Log.e("---", "char：$char")
-            BluetoothHelper.sendCommandAndWaitForResponse("AT+VERSION=?\r\n")
-//            BluetoothLEUtil.sendCommandWithNotification(
-//                BluetoothHelper.getCurrentGate()!!,
-//                uuid,
-//                char,
-//                "AT+VERSION=?\r\n"
-//            )
-//            CommandUtil.readResponse(BluetoothHelper.getCurrentGate()!!, uuid, char) {
-//                Log.e("---", "resp：$it")
-//            }
-        }
-
-        binding.ll2.setOnClickListener {
-//            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-//                CommandUtil.sendCommand(it, "AT+REBOOT")
-//                ResultDialog.newInstance(childFragmentManager, "成功", "")
-//            }
-            val uuid = KeyValueUtils.getString(IConsts.KEY_CURRENT_WRITE_UUID)
-            val char = KeyValueUtils.getString(IConsts.KEY_CURRENT_WRITE_CHARACTERISTICS)
-            CommandUtil.sendCommand(ConnectUtil.CURRENT_GATE!!, uuid, char, "AT+REBOOT\r")
-        }
-
-        binding.ll3.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                CommandUtil.sendCommand(it, "AT+CLEAR")
-                ResultDialog.newInstance(childFragmentManager, "成功", "")
-            }
-        }
-
-
-        binding.ll4.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                CommandUtil.sendCommand(it, "AT+SAVE")
-                ResultDialog.newInstance(childFragmentManager, "成功", "")
-            }
-        }
-
-        binding.ll5.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                val res = CommandUtil.sendCommandWithResponse(it, "AT+STATUS=?")
-                if (TextUtils.isEmpty(res)) {
-                    ResultDialog.newInstance(childFragmentManager, "", "")
-                } else {
-                    ResultDialog.newInstance(childFragmentManager, res!!, "")
-                }
-            }
-        }
-
-        binding.ll6.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                val res = CommandUtil.sendCommandWithResponse(it, "AT+STATE=?")
-                if (TextUtils.isEmpty(res)) {
-                    ResultDialog.newInstance(childFragmentManager, "", "")
-                } else {
-                    ResultDialog.newInstance(childFragmentManager, res!!, "")
-                }
-            }
-        }
-
-        binding.ll7.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                val res = CommandUtil.sendCommandWithResponse(it, "AT+MEMS=?")
-                if (TextUtils.isEmpty(res)) {
-                    ResultDialog.newInstance(childFragmentManager, "", "")
-                } else {
-                    ResultDialog.newInstance(childFragmentManager, res!!, "")
-                }
-            }
-        }
-
-        binding.ll8.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                val res = CommandUtil.sendCommandWithResponse(it, "AT+ICCID=?")
-                if (TextUtils.isEmpty(res)) {
-                    ResultDialog.newInstance(childFragmentManager, "", "")
-                } else {
-                    ResultDialog.newInstance(childFragmentManager, res!!, "")
-                }
-            }
-        }
-
-        binding.ll9.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                val res = CommandUtil.sendCommandWithResponse(it, "AT+CCLK=?")
-                if (TextUtils.isEmpty(res)) {
-                    ResultDialog.newInstance(childFragmentManager, "", "")
-                } else {
-                    ResultDialog.newInstance(childFragmentManager, res!!, "")
-                }
-            }
-        }
-
-        binding.ll10.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                val res = CommandUtil.sendCommandWithResponse(it, "AT+CSQ/4G=?")
-                if (TextUtils.isEmpty(res)) {
-                    ResultDialog.newInstance(childFragmentManager, "", "")
-                } else {
-                    ResultDialog.newInstance(childFragmentManager, res!!, "")
-                }
-            }
-        }
-
-        binding.ll11.setOnClickListener {
-            ConnectUtil.CURRENT_BLUE_SOCKET?.let {
-                CommandUtil.sendCommand(it, "AT+RST/WIFI")
-                ResultDialog.newInstance(childFragmentManager, "成功", "")
-            }
-        }
-
-
-        binding.tvConnect.setOnClickListener {
-            PermissionComplianceManager.requestFineLocationPermissionHasTip(
-                requireActivity(),
-                object : PermissionComplianceManager.SimpleCallbackProxy() {
-                    @SuppressLint("MissingPermission")
-                    override fun onGranted() {
-                        BlueToothListDialog.newInstance(childFragmentManager, devices)
-                        BluetoothHelper.startScan({ device ->
-                            if (!deviceList.contains(device) && !TextUtils.isEmpty(device.name)) {
-                                deviceList.add(device)
-                                val deviceInfo = BlueToothBean(name = device.name, address = device.address, device)
-//                                devices.add(deviceInfo)
-                                BlueToothListDialog.notify(deviceInfo)
-                            }
-                        }, { errorCode ->
-                            Toast.makeText(context,"扫描失败", Toast.LENGTH_SHORT).show()
-                        })
-                    }
-                })
-        }
-
-        binding.tvDisconnect.setOnClickListener {
-//            ConnectUtil.unpairBluetoothDevice(ConnectUtil.CURRENT_DEVICE)
-            BluetoothHelper.disconnect()
-        }
-        binding.tvLink.setOnClickListener {
-/*            ConnectUtil.connectBLEDevice(requireContext(), ConnectUtil.getCurrentDevice()!!,object : BluetoothGattCallback() {
-                override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
-                    when (newState) {
-                        BluetoothProfile.STATE_CONNECTED -> {
-                            Log.d("BLEConnection", "设备已连接")
-                            gatt.discoverServices()  // 开始发现服务
-                            binding.tvLink.visibility = View.GONE
-                        }
-                        BluetoothProfile.STATE_DISCONNECTED -> {
-                            Log.d("BLEConnection", "设备已断开连接")
-                        }
-                    }
-                }
-
-                override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-                    if (status == BluetoothGatt.GATT_SUCCESS) {
-                        Log.d("BLEConnection", "服务发现成功")
-                        val services = gatt.services
-                        for (service in services) {
-                            Log.d("BluetoothGatt", "发现服务: ${service.uuid}")
-                            KeyValueUtils.setString(IConsts.KEY_CURRENT_WRITE_UUID, service.uuid.toString())
-                            // 获取服务中的所有特性
-                            val characteristics = service.characteristics
-                            for (characteristic in characteristics) {
-                                val characteristicUUID = characteristic.uuid
-                                Log.d("BluetoothGatt", "发现特性: $characteristicUUID")
-                                KeyValueUtils.setString(IConsts.KEY_CURRENT_WRITE_CHARACTERISTICS, characteristicUUID.toString())
-
-
-                                // 检查该特性是否支持写入
-                                if (characteristic.properties and BluetoothGattCharacteristic.PROPERTY_WRITE != 0) {
-                                    Log.d("BluetoothGatt", "特性支持写入: $characteristicUUID")
-                                }
-
-                                // 检查该特性是否支持读取
-                                if (characteristic.properties and BluetoothGattCharacteristic.PROPERTY_READ != 0) {
-                                    Log.d("BluetoothGatt", "特性支持读取: $characteristicUUID")
-                                }
-                            }
-                        }
-                    } else {
-                        Log.d("BLEConnection", "服务发现失败: $status")
-                    }
-                }
-            })*/
-            ConnectUtil.CURRENT_GATE = BluetoothLEUtil.connectToDevice(
-                requireContext(),
-                ConnectUtil.getCurrentDevice()!!,
-                BluetoothLEUtil.gattCallback
-            )
-        }
     }
 
     private fun initData() {
@@ -274,36 +57,16 @@ class HomeFragment : Fragment() {
             while (true) {
                 delay(1000)
                 if (lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) {
-                    refreshPageBlueToothState()
                 }
             }
         }
     }
 
 
-    private fun refreshPageBlueToothState() {
-        if (BluetoothHelper.isAnyDeviceConnected(requireContext())) {
-            binding.tvStatus.text = "蓝牙已连接"
-            binding.tvConnect.visibility = View.GONE
-            binding.tvDisconnect.visibility = View.VISIBLE
-//            ConnectUtil.getCurrentSocket()
-//            if (bluetoothHelper.isAnyDeviceConnected(requireContext())) {
-//                binding.tvLink.visibility = View.GONE
-//            } else {
-//                binding.tvLink.visibility = View.VISIBLE
-//            }
-        } else {
-            binding.tvStatus.text = "蓝牙未连接"
-            binding.tvConnect.visibility = View.VISIBLE
-            binding.tvDisconnect.visibility = View.GONE
-            binding.tvLink.visibility = View.GONE
-        }
-    }
 
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        BluetoothHelper.disconnect()
     }
 }
